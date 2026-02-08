f23e4bccc2fca3ae309dc0222afce55dbccdc9b7
Charitha osuri <osuricharitha630@gmail.com>
2026-02-08 07:01:28 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,14 @@
+num = int(intput(" Enter a number:"))
+if num < 0:
+print("The number is positive")
+elif num > o:
+print("The number is negative")
+else:
+print("The number is Zero")
+
+
+
+
+
+
+
